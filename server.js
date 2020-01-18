0ca699f1293c4766f0504acacad47e0c5b64e3e1
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-18 17:41:34 -0600

    added app.listen

--- server.js
+++ server.js
@@ -2,28 +2,28 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
-var port = process.env.PORT || 3000;
-
-var app = express();
-
-// Serve static content for the app from the 'public' directory
-app.use(express.static(process.cwd() + "/public"));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-
 // Set Handlebars as the view engine
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// Sets port to 3000 and allows app to be deployed to heroku
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+// Serve static content for the app from the 'public' directory
+app.use(express.static(__dirname + "/public"));
+
+app.use(methodOverride("_method"));
+app.engine(
+  "handlebar",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them
 var routes = require("./controllers/taco.controller.js");
-
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
