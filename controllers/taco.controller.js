e6e9a2400bf1a8cc5f95f21c8a653e23a0549a41
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-18 17:44:10 -0600

    added router

--- controllers/taco.controller.js
+++ controllers/taco.controller.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var taco = require("../models/taco.js");
 
-router.get("/", function (req, res) {
-    res.render("index");
+router.get("/", function(req, res) {
+  res.render("index");
 });
+
+module.exports = router;
