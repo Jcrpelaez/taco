44d78bf853ac53c5ab8edb521e9db25439a1e020
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-25 18:30:06 -0600

    added true values

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,5 @@
 INSERT INTO tacos (taco_name, devoured) VALUES ('Bean & Cheese', false);
 INSERT INTO tacos (taco_name, devoured) VALUES ('Barbacoa', false);
 INSERT INTO tacos (taco_name, devoured) VALUES ('Potato & Egg', false);
+INSERT INTO tacos (taco_name, devoured) VALUES ('Bacon & Egg', true);
+INSERT INTO tacos (taco_name, devoured) VALUES ('Sausage & Egg', true);
